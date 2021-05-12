8d74628eccfd9b35410a300fc4605ff05f849199
Tiara Feaster <tfeaster21@gmail.com>
2021-05-12 15:25:22 -0400

    components part 1
    added directory and main components
    updated app.js

--- App.js
+++ App.js
@@ -1,21 +1,6 @@
-import { StatusBar } from "expo-status-bar";
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import Main from "./components/MainComponent";
 
 export default function App() {
-	return (
-		<View style={styles.container}>
-			<Text> Hello World!</Text>
-			<StatusBar style="auto" />
-		</View>
-	);
+	return <Main />;
 }
-
-const styles = StyleSheet.create({
-	container: {
-		flex: 1,
-		backgroundColor: "#fff",
-		alignItems: "center",
-		justifyContent: "center",
-	},
-});
